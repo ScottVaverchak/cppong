69f563bb10bd7a5faacff1061b7663f347061ea4
Scott Vaverchak <Scott.Vaverchak@gmail.com>
2020-03-08 15:44:21 -0400

    player is bound to game world

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include <SDL2/SDL.h>
 #include <cstdio>
 #include <cstdint>
+#include <cassert>
+
 
 const int WINDOW_W = 800;
 const int WINDOW_H = 400;
-const int PLAYER_W = 25;
-const int PLAYER_H = 100;
-const int PLAYER_X = 10;
-const int GAMEAREA_W = WINDOW_W;
-const int GAMEAREA_H = WINDOW_H;
-const int GAMEAREA_X = 0;
-const int GAMEAREA_Y = 0;
+
+const int GAMEAREA_W = WINDOW_W * 0.9f;
+const int GAMEAREA_H = WINDOW_H * 0.75f;
+const int GAMEAREA_X = (WINDOW_W - GAMEAREA_W) / 2;
+const int GAMEAREA_Y = (WINDOW_H - GAMEAREA_H) / 2;
 
 int SDL_ErrorCheck(int code) {
     if(code != 0) {
@@ -36,7 +36,13 @@ void draw_colored_rectangle(SDL_Renderer *renderer, SDL_Rect rect, uint32_t colo
     // R G B A
     Uint8 r, g, b, a;
     SDL_ErrorCheck(SDL_GetRenderDrawColor(renderer, &r, &g, &b, &a));
-    SDL_ErrorCheck(SDL_SetRenderDrawColor(renderer, (color & 0xFF000000) >> 24, (color & 0x00FF0000) >> 16, (color & 0x0000FF00) >> 8, (color & 0x000000FF)));
+    
+    SDL_ErrorCheck(SDL_SetRenderDrawColor(renderer, 
+        (color & 0xFF000000) >> 24, 
+        (color & 0x00FF0000) >> 16, 
+        (color & 0x0000FF00) >> 8, 
+        (color & 0x000000FF)));
+
     SDL_ErrorCheck(SDL_RenderDrawRect(renderer, &rect));
     SDL_ErrorCheck(SDL_SetRenderDrawColor(renderer, r, g, b, a));
 }
@@ -44,6 +50,19 @@ void draw_colored_rectangle(SDL_Renderer *renderer, SDL_Rect rect, uint32_t colo
 int main(int argc, char** argv) {
     printf("Initializing SDL...\n");
   
+    const int PLAYER_W = 25;
+    assert((PLAYER_W * 2) < GAMEAREA_W);
+    
+    const int PLAYER_H = 100;
+    assert(PLAYER_H < GAMEAREA_H);
+
+    const int PLAYER_OFFSET_X = 5;
+    const int PLAYER_X = GAMEAREA_X + PLAYER_OFFSET_X;
+    assert(PLAYER_X > GAMEAREA_X && PLAYER_X < (GAMEAREA_W + GAMEAREA_X));
+
+    const int PLAYER_Y = (GAMEAREA_H) / 2;
+    assert(PLAYER_Y > GAMEAREA_Y && PLAYER_Y < (GAMEAREA_H + GAMEAREA_Y));
+
     SDL_ErrorCheck(SDL_Init(SDL_INIT_VIDEO));
     SDL_Window *window = SDL_ErrorCheck(SDL_CreateWindow("cppong", 
                                                           SDL_WINDOWPOS_UNDEFINED, 
@@ -57,7 +76,7 @@ int main(int argc, char** argv) {
 
     bool quit = false;         
     SDL_Rect world = {GAMEAREA_X, GAMEAREA_Y, GAMEAREA_W, GAMEAREA_H};
-    SDL_Rect player = {PLAYER_X, 10, PLAYER_W, PLAYER_H};
+    SDL_Rect player = {PLAYER_X, PLAYER_Y, PLAYER_W, PLAYER_H};
     SDL_Event e;
 
     Uint32 prev_dt = 1;
@@ -79,7 +98,7 @@ int main(int argc, char** argv) {
             dy = 1;
         }
 
-        if(player.y + dy >= 0 && (player.y + dy + player.h) < 400)
+        if(player.y + dy >= GAMEAREA_Y && (player.y + dy + player.h) < GAMEAREA_Y + GAMEAREA_H)
             player.y += dy;
 
         SDL_ErrorCheck(SDL_RenderClear(renderer));
