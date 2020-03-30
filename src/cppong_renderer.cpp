8020a4065aa287efe850100f0f92421ce28174fa
Scott Vaverchak <Scott.Vaverchak@gmail.com>
2020-03-30 17:45:29 -0400

    more small changes

--- src/cppong_renderer.cpp
+++ src/cppong_renderer.cpp
@@ -47,60 +47,57 @@ void render_border(SDL_Renderer *renderer, SDL_Rect gamearea, SDL_Texture *sprit
     // @TODO(sjv): This looks like it could be optimized more
     // @NOTE(sjv): Maybe this should be its own data struct for 9 slices
 
-    SDL_Rect srctl = { 16 * 2, 16 * 0, 16, 16 };
-    SDL_Rect srctm = { 16 * 3, 16 * 0, 16, 16 };
-    SDL_Rect srctr = { 16 * 4, 16 * 0, 16, 16 };
-    SDL_Rect srcml = { 16 * 2, 16 * 1, 16, 16 };
-    SDL_Rect srcmr = { 16 * 4, 16 * 1, 16, 16 };
-    SDL_Rect srcbl = { 16 * 2, 16 * 2, 16, 16 };
-    SDL_Rect srcbm = { 16 * 3, 16 * 2, 16, 16 };
-    SDL_Rect srcbr = { 16 * 4, 16 * 2, 16, 16 };
+    SDL_Rect srctl { 16 * 2, 16 * 0, 16, 16 };
+    SDL_Rect srctm { 16 * 3, 16 * 0, 16, 16 };
+    SDL_Rect srctr { 16 * 4, 16 * 0, 16, 16 };
+    SDL_Rect srcml { 16 * 2, 16 * 1, 16, 16 };
+    SDL_Rect srcmr { 16 * 4, 16 * 1, 16, 16 };
+    SDL_Rect srcbl { 16 * 2, 16 * 2, 16, 16 };
+    SDL_Rect srcbm { 16 * 3, 16 * 2, 16, 16 };
+    SDL_Rect srcbr { 16 * 4, 16 * 2, 16, 16 };
 
     //  TOP
-    SDL_Rect dsttl = {gamearea.x - 16, gamearea.y - 16, 16, 16};
+    SDL_Rect dsttl {gamearea.x - 16, gamearea.y - 16, 16, 16};
     SDL_ErrorCheck(SDL_RenderCopyEx(renderer, spritesheet, 
                                     &srctl, &dsttl, 0,  
                                     nullptr, SDL_FLIP_NONE));
 
-    SDL_Rect dsttm = {gamearea.x, gamearea.y - 16, gamearea.w, 16};
+    SDL_Rect dsttm {gamearea.x, gamearea.y - 16, gamearea.w, 16};
     SDL_ErrorCheck(SDL_RenderCopyEx(renderer, spritesheet, 
                                     &srctm, &dsttm, 0,  
                                     nullptr, SDL_FLIP_NONE));
     
-    SDL_Rect dsttr = {gamearea.x + gamearea.w, gamearea.y - 16, 16, 16};
+    SDL_Rect dsttr {gamearea.x + gamearea.w, gamearea.y - 16, 16, 16};
     SDL_ErrorCheck(SDL_RenderCopyEx(renderer, spritesheet, 
                                     &srctr, &dsttr, 0,  
                                     nullptr, SDL_FLIP_NONE));
 
     // MIDDLE
-    SDL_Rect dstml = {gamearea.x - 16, gamearea.y, 16, gamearea.h};
+    SDL_Rect dstml {gamearea.x - 16, gamearea.y, 16, gamearea.h};
     SDL_ErrorCheck(SDL_RenderCopyEx(renderer, spritesheet, 
                                     &srcml, &dstml, 0,  
                                     nullptr, SDL_FLIP_NONE));
 
-    SDL_Rect dstmr = {gamearea.x + gamearea.w, gamearea.y, 16, gamearea.h};
+    SDL_Rect dstmr {gamearea.x + gamearea.w, gamearea.y, 16, gamearea.h};
     SDL_ErrorCheck(SDL_RenderCopyEx(renderer, spritesheet, 
                                     &srcmr, &dstmr, 0,  
                                     nullptr, SDL_FLIP_NONE));
 
     // BOTTOM
-    SDL_Rect dstbl = {gamearea.x - 16, gamearea.y + gamearea.h, 16, 16};
+    SDL_Rect dstbl {gamearea.x - 16, gamearea.y + gamearea.h, 16, 16};
     SDL_ErrorCheck(SDL_RenderCopyEx(renderer, spritesheet, 
                                     &srcbl, &dstbl, 0,  
                                     nullptr, SDL_FLIP_NONE));
 
-    SDL_Rect dstbm = {gamearea.x, gamearea.y + gamearea.h, gamearea.w, 16};
+    SDL_Rect dstbm {gamearea.x, gamearea.y + gamearea.h, gamearea.w, 16};
     SDL_ErrorCheck(SDL_RenderCopyEx(renderer, spritesheet, 
                                     &srcbm, &dstbm, 0,  
                                     nullptr, SDL_FLIP_NONE));
 
-    SDL_Rect dstbr = {gamearea.x + gamearea.w, gamearea.y + gamearea.h, 16, 16};
+    SDL_Rect dstbr {gamearea.x + gamearea.w, gamearea.y + gamearea.h, 16, 16};
     SDL_ErrorCheck(SDL_RenderCopyEx(renderer, spritesheet, 
                                     &srcbr, &dstbr, 0,  
                                     nullptr, SDL_FLIP_NONE));
 
 }
 
-
-
-// void render_end() {}
