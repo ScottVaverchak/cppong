04a25a2f9442e7e4be9d1a63747fc1f9f485de0e
Scott Vaverchak <Scott.Vaverchak@gmail.com>
2020-03-31 16:31:25 -0400

    win32 using local libs

--- win32build.bat
+++ win32build.bat
@@ -5,9 +5,9 @@
 @xcopy "assets" "build\assets\" /E /Y /F 
 @pushd build
 
-cl.exe /EHsc /MD  ..\src\win32_cppong.cpp /I ..\libs\win32\sdl2\include /I ..\libs\win32\sdl2_ttf\include /link /LIBPATH:..\libs\win32\sdl2\lib\x86 SDL2.lib SDL2main.lib /LIBPATH:..\libs\win32\sdl2_ttf\lib\x86 SDL2_ttf.lib /SUBSYSTEM:CONSOLE
+cl.exe /EHsc /MD  ..\src\win32_cppong.cpp /I ..\..\libs\sdl2\include /I ..\..\libs\sdl2_ttf\include /link /LIBPATH:..\..\libs\sdl2\lib\x86 SDL2.lib SDL2main.lib /LIBPATH:..\..\libs\sdl2_ttf\lib\x86 SDL2_ttf.lib /SUBSYSTEM:CONSOLE
 
-@copy ..\libs\win32\sdl2_ttf\lib\x86\*.dll .
-@copy ..\libs\win32\sdl2\lib\x86\*.dll .
+@copy ..\..\libs\sdl2_ttf\lib\x86\*.dll .
+@copy ..\..\libs\sdl2\lib\x86\*.dll .
 
 @popd
